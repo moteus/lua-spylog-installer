f1b2669796a6ae6049722cd2c027d7dac11107b5
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-10-19 10:26:39 +0300

    Remove unused files.

--- win/spylog/config/jails/access.lua
+++
@@ -1,8 +0,0 @@
-JAIL{"fail-access";
-  enabled  = true;
-  filter   = {"rdp-fail-access", "fusionpbx-fail-access", "radmin-fail-access"};
-  findtime = 300;
-  maxretry = 4;
-  bantime  = 600;
-  action   = {"ipsec", {"mail", {unban=false}}};
-}
